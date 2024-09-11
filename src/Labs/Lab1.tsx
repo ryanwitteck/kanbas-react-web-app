91817e1b3e6eefbe2891d8e61986029c327bccc3
Ryan Witteck <witteck.r@northeastern.edu>
2024-09-10 20:35:07 -0400

    a1 change

--- src/Labs/Lab1.tsx
+++ src/Labs/Lab1.tsx
@@ -2,8 +2,9 @@ export default function Lab1(){
     return(
         <div>
         <h2>Lab 1</h2>
-        <h3>Exercise 1.1</h3>
-        <h3>Exercise 1.2</h3>
+        <h3>Headings</h3>
+        <h1>H1 is the largest header</h1>
+        <h6>and H6 is the smallest</h6>
         </div>
 
     );
