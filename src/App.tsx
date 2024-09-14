d22432d6287e278fc16e6e67742821e4df1a455c
Ryan Witteck <witteck.r@northeastern.edu>
2024-09-14 15:39:58 -0400

    Lab1 Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Labs from './Labs';
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <Labs />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+        </Routes>
+      </div>
+    </HashRouter>
+
   );
 }
 
