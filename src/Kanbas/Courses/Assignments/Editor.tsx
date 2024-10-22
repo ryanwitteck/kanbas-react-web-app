0c9b073bd7c5ffaa9e769b513dcfefe1f299e8eb
Ryan Witteck <witteck.r@northeastern.edu>
2024-10-22 10:02:23 -0400

    fixed dates and times A3

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -6,27 +6,27 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 export default function AssignmentEditor() {
     const { cid, aid } = useParams();
-    
+
     const assignment = db.assignments.filter((module: any) => module._id === aid);
     const defaultAssignment = (assignment[0] && assignment[0].group && assignment[0].group === "ASSIGNMENTS") ? true : false;
-    const defaultQuiz = (assignment[0] && assignment[0].group && assignment[0].group === "QUIZZES")? true : false;
-    const defaultExam = (assignment[0] && assignment[0].group && assignment[0].group === "EXAMS")? true : false;
-    const defaultProject = (assignment[0] && assignment[0].group && assignment[0].group === "PROJECT")? true : false;
+    const defaultQuiz = (assignment[0] && assignment[0].group && assignment[0].group === "QUIZZES") ? true : false;
+    const defaultExam = (assignment[0] && assignment[0].group && assignment[0].group === "EXAMS") ? true : false;
+    const defaultProject = (assignment[0] && assignment[0].group && assignment[0].group === "PROJECT") ? true : false;
 
-    const defaultPercentage = (assignment[0] && assignment[0].display_grade_as && assignment[0].display_grade_as === "PERCENTAGE")? true : false;
-    const defaultDecimal = (assignment[0] && assignment[0].display_grade_as && assignment[0].display_grade_as === "DECIMAL")? true : false;
-    const defaultLetter = (assignment[0] && assignment[0].display_grade_as && assignment[0].display_grade_as === "LETTER")? true : false;
-    
-    const defaultOnline = (assignment[0] && assignment[0].submission_type && assignment[0].submission_type === "ONLINE")? true : false;
-    const defaultInPerson = (assignment[0] && assignment[0].submission_type && assignment[0].submission_type === "INPERSON")? true : false;
-    
+    const defaultPercentage = (assignment[0] && assignment[0].display_grade_as && assignment[0].display_grade_as === "PERCENTAGE") ? true : false;
+    const defaultDecimal = (assignment[0] && assignment[0].display_grade_as && assignment[0].display_grade_as === "DECIMAL") ? true : false;
+    const defaultLetter = (assignment[0] && assignment[0].display_grade_as && assignment[0].display_grade_as === "LETTER") ? true : false;
 
-    const defaultTextEntry = (assignment[0] && assignment[0].online_entry_type && assignment[0].online_entry_type.text)? true : false; // Need redundant if-else for type checking later, compiler complains
-    const defaultURL = (assignment[0] && assignment[0].online_entry_type && assignment[0].online_entry_type.url)? true : false;
-    const defaultMedia = (assignment[0] && assignment[0].online_entry_type && assignment[0].online_entry_type.media)? true : false;
-    const defaultAnnotations = (assignment[0] && assignment[0].online_entry_type && assignment[0].online_entry_type.annotations)? true : false;
-    const defaultFileUpload = (assignment[0] && assignment[0].online_entry_type && assignment[0].online_entry_type.file_upload)? true : false;
-    
+    const defaultOnline = (assignment[0] && assignment[0].submission_type && assignment[0].submission_type === "ONLINE") ? true : false;
+    const defaultInPerson = (assignment[0] && assignment[0].submission_type && assignment[0].submission_type === "INPERSON") ? true : false;
+
+
+    const defaultTextEntry = (assignment[0] && assignment[0].online_entry_type && assignment[0].online_entry_type.text) ? true : false; // Need redundant if-else for type checking later, compiler complains
+    const defaultURL = (assignment[0] && assignment[0].online_entry_type && assignment[0].online_entry_type.url) ? true : false;
+    const defaultMedia = (assignment[0] && assignment[0].online_entry_type && assignment[0].online_entry_type.media) ? true : false;
+    const defaultAnnotations = (assignment[0] && assignment[0].online_entry_type && assignment[0].online_entry_type.annotations) ? true : false;
+    const defaultFileUpload = (assignment[0] && assignment[0].online_entry_type && assignment[0].online_entry_type.file_upload) ? true : false;
+
     return (
         <div id="wd-assignments-editor" className="container mt-4">
             <div className="row mb-3">
@@ -114,11 +114,11 @@ export default function AssignmentEditor() {
                         </div>
                         <div className=" d-flex wd-flex-row-container justify-content-center align-items-center ">
                             <input type="checkbox" id="wd-text-entry" checked={defaultTextEntry} className="wd-button-type" />
-                            <label htmlFor="wd-text-entry"  className="wd-button-text-type">&nbsp; Text Entry</label>
+                            <label htmlFor="wd-text-entry" className="wd-button-text-type">&nbsp; Text Entry</label>
                         </div>
                         <div className=" d-flex wd-flex-row-container justify-content-center align-items-center ">
-                            <input type="checkbox" id="wd-website-url" checked={defaultURL}  className="wd-button-type-url" />
-                            <label htmlFor="wd-website-url"  className="wd-button-text-type-url">&nbsp; Website URL</label>
+                            <input type="checkbox" id="wd-website-url" checked={defaultURL} className="wd-button-type-url" />
+                            <label htmlFor="wd-website-url" className="wd-button-text-type-url">&nbsp; Website URL</label>
                         </div>
                         <div className=" d-flex wd-flex-row-container justify-content-center align-items-center ">
                             <input type="checkbox" id="wd-media-recordings" checked={defaultMedia} className="wd-button-type-mr" />
@@ -157,21 +157,31 @@ export default function AssignmentEditor() {
                             </div>
                         </div>
                         <div className=" d-flex wd-flex-row-container justify-content-center align-items-center ">
-                            <input type="date" id="wd-due-date" value={`${(assignment[0] && assignment[0].due_date) ? assignment[0].due_date : ``}`} className="form-control" />
+                            <input type="datetime-local" id="wd-due-date" value={`${(assignment[0] && assignment[0].due_date) ? assignment[0].due_date : ``}`} className="form-control" style={{ fontSize: '12px' }} />
                         </div>
 
-                        <div className=" d-flex wd-flex-row-container justify-content-center align-items-center">
+
+
+
+                        <div className=" d-flex wd-flex-row-container justify-content-left align-items-center">
                             <div className="col-md-4">
-                                <label style={{ width: '100px' }} htmlFor="wd-available-from" className=""><b>Available From</b></label>
-                            </div>
-                            <div className="col-md-6">
-                                <label style={{ width: '100px' }} htmlFor="wd-available-until" className="wd-until"><b>Until</b></label>
+                            <label style={{ width: '200px' }} htmlFor="wd-available-from" className=""><b>Available From</b></label>
                             </div>
                         </div>
-                        <div className=" d-flex wd-flex-row-container justify-content-start align-items-center " style={{ gap: '20px' }}>
-                            <input style={{ width: '100px' }} type="date" id="wd-available-from" value={`${(assignment[0] && assignment[0].available_to) ? assignment[0].available_to : ``}`} className="form-control" />
-                            <input style={{ width: '100px' }} type="date" id="wd-available-until" value={`${(assignment[0] && assignment[0].available_from) ? assignment[0].available_from : ``}`} className="form-control" />
+                        <div className=" d-flex wd-flex-row-container justify-content-center align-items-center ">
+                        <input style={{ width: '220px', fontSize: '12px' }} type="datetime-local" id="wd-available-from" value={`${(assignment[0] && assignment[0].available_to) ? assignment[0].available_to : ``}`} className="form-control" />
                         </div>
+
+
+                        <div className=" d-flex wd-flex-row-container justify-content-left align-items-left">
+                            <div className="col-md-4">
+                            <label style={{ width: '100px', position: "relative", left:"-0px" }} htmlFor="wd-available-until" className="wd-until"><b>Until</b></label>
+                            </div>
+                        </div>
+                        <div className=" d-flex wd-flex-row-container justify-content-left align-items-center ">
+                        <input style={{ width: '220px', fontSize: '12px' }} type="datetime-local" id="wd-available-until" value={`${(assignment[0] && assignment[0].available_from) ? assignment[0].available_from : ``}`} className="form-control" />
+                        </div>
+
                     </div>
                 </div>
             </div>
